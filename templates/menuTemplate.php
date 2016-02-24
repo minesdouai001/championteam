ae0c1dc46c82d07f1bc9eb576d01040d3cd13984
MOURAD <MOURAD@MOURAD-PC>
2016-02-24 10:20:17 +0100

    Formulaire connexion

--- templates/menuTemplate.php
+++ templates/menuTemplate.php
@@ -1,33 +1,41 @@
 
 <nav class="navbar navbar-default navbar-fixed-top">
-<div class="container">
-<div class="navbar-header">
-<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-<span class="sr-only">Toggle navigation</span>
-<span class="icon-bar"></span>
-<span class="icon-bar"></span>
-<span class="icon-bar"></span>
-</button>
-<a class="navbar-brand" href="."> Ch'min'ots </a>
-</div>
-<div id="navbar" class="collapse navbar-collapse">
-<ul class="nav navbar-nav">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href=".">  </a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
 
-<li><a href="index.php?action=leJeu">FAQ</a></li>
-<li><a href="index.php?action=statistiques">Statistiques</a></li>
+                <li><a href="index.php?action=leJeu">FAQ</a></li>
 
-<li><a href="index.php?action=nousContacter">Nous contacter</a></li>
-<ul class="nav navbar-nav navbar-right">
-<li class="dropdown">
+            </ul>   
+            
+                <form class="form-signina" action="index.php?action=connexion" method="post">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>                        
+                        <?php
+                        if (isset($conErrorText))
+                            echo ' <span class="error"> ' . $conErrorText . ' </span> <br>';
+                        ?>         
+                        <input type="text" name="conLogin" class="form-inline" placeholder="Login" value="<?php if (isset($_POST['conLogin'])) echo $_POST['conLogin'] ?>" required autofocus> 
+                        <input type="password" name="conPassword" class="form-inline" placeholder="Mot de passe" required>   
+                        <button class="btn btn-primary" type="submit" style="margin-top: 5px;">Connexion</button><br>
+                    </ul>
+                </form>
+            
+            
 
-<li><a href="index.php">Connexion</a></li>
-</li>
-	</ul>
-</ul>
-</div><!--/.nav-collapse -->
+        </div><!--/.nav-collapse -->
 
 
-</div>
+    </div>
 </nav>
 
 
